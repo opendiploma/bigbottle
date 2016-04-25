26d3d806bf67bb7af13fcba5da457b795f954d7e
IgorRuzanov <igor.ruzanov@pearlchain.net>
2016-04-25 12:15:05 +0200

    fix

--- bigbottle.py
+++ bigbottle.py
@@ -1,32 +1,42 @@
-from bottle import route, request
+from bottle import get, request, post
 import bigchain_client
 import json
 
 
-@route('/keys')
+@get('/keys')
 def keys():
     user_priv, user_pub = bigchain_client.generate_key_pair()
     return json.dumps({'private': user_priv, 'public': user_pub})
 
 
-@route('/store', method='POST')
+@post('/store')
 def store():
-    public_key = request.POST.get('pub', '').strip()
-    asset = request.POST.get('asset', '').strip()
+    public_key = request.forms.get('pub', '').strip()
+    if not public_key:
+        public_key = request.GET.get('pub', '').strip()
 
-    digital_asset_payload = {'asset': asset}
+    asset = request.forms.get('asset', '').strip()
+    if not asset:
+        asset = request.GET.get('asset', '').strip()
 
-    return bigchain_client.store_asset(public_key, digital_asset_payload)
+    digital_asset_payload = get_asset_payload(asset)
+
+    store_result = bigchain_client.store_asset(public_key, digital_asset_payload)
+
+    return json.dumps({'pub': public_key, 'asset': digital_asset_payload, 'result': store_result})
 
 
-@route('/exists', method='GET')
+@get('/exists')
 def exists():
     asset = request.GET.get('asset', '').strip()
 
-    digital_asset_payload = {'asset': asset}
+    digital_asset_payload = get_asset_payload(asset)
     result = bigchain_client.exists(digital_asset_payload)
 
     return json.dumps({'result': result})
 
+
+def get_asset_payload(asset):
+    return {'asset': asset}
 
 # run(host='localhost', port=8777, debug=True)
