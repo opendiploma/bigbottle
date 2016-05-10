5dc2b87af8b1cee87fda27a05683792e837ab42b
IgorRuzanov <igor.ruzanov@pearlchain.net>
2016-05-10 17:03:54 +0300

    Vagrant fix

--- vagrant/config.sh
+++ vagrant/config.sh
@@ -5,21 +5,15 @@ apt-get -qqy install build-essential
 apt-get -qqy install curl
 apt-get -qqy install git
 apt-get -qqy install libssl-dev
-apt-get -qqy install python3-pip
+apt-get -qqy install g++ python3-dev python3-setuptools
 apt-get -qqy install wget
 apt-get -qqy install ca-certificates libffi-dev
-apt-get -qqy install g++ python3-dev
-apt-get -qqy install python3-setuptools
 
 easy_install3 pip
+pip install --upgrade setuptools
 
 #install bigchaindb
-wget https://github.com/bigchaindb/bigchaindb/archive/v0.3.0.tar.gz
-    tar -xvzf v0.3.0.tar.gz && \
-    rm -Rf v0.3.0.tar.gz && \
-    cd bigchaindb-0.3.0 && \
-    python3 setup.py install && \
-rm -rf /var/cache/apk/*
+pip3 install bigchaindb
 
 # install rethinkdb
 source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
