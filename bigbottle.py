0a7ea12117888e52c95f31c01cece1d6e7ceba47
IgorRuzanov <igor.ruzanov@pearlchain.net>
2016-05-18 13:36:34 +0200

    demo

--- bigbottle.py
+++ bigbottle.py
@@ -1,6 +1,12 @@
-from bottle import get, request, post
+from bottle import get, request, post, static_file, route
 import bigchain_client
 import json
+
+user_public_key = '7By7xQPn1gPsqCeAqSJXcLGqukobmJfsGa1ovczTvibh'
+user_private_key = '845zJ4tQZUo4jNCBfYzZhWPBrt2Akwi2fptLpWnnE2uW'
+
+univer_public_key = 'ACUTcyq9YzodJRoU2LgTAKe2VXB59KYogvMDB1KLkN5R'
+univer_private_key = '8rnGzzC4doGghk3aHe4uw3CLX9xCZhFoakzDWKMZqX9H'
 
 
 @get('/keys')
@@ -11,14 +17,14 @@ def keys():
 
 @post('/store')
 def store():
-    public_key = get_request_param('pub')
+    public_key = get_request_param('pub') or user_public_key
     asset = get_request_param('asset')
 
     digital_asset_payload = get_asset_payload(asset)
 
     store_result = bigchain_client.store_asset(public_key, digital_asset_payload)
 
-    return json.dumps({'pub': public_key, 'asset': digital_asset_payload, 'result': store_result})
+    return json.dumps({'pub': public_key, 'asset': digital_asset_payload, 'transaction': store_result})
 
 
 @get('/exists')
@@ -26,9 +32,37 @@ def exists():
     asset = request.GET.get('asset', '').strip()
 
     digital_asset_payload = get_asset_payload(asset)
-    result = bigchain_client.exists(digital_asset_payload)
+    result, transactions = bigchain_client.exists(digital_asset_payload)
 
-    return json.dumps({'result': result})
+    return json.dumps({'result': result, 'asset': asset, 'transactions': transactions})
+
+
+@get('/owned')
+def get_owned():
+    pub = request.GET.get('pub', '').strip() or user_public_key
+
+    transactions = bigchain_client.get_owned_ids(pub)
+
+    return json.dumps(transactions)
+
+
+@post('/sign')
+def sign():
+    public_key = get_request_param('pub') or user_public_key
+    private_key = get_request_param('priv') or user_private_key
+    send_to_key = get_request_param('send_to') or univer_public_key
+    tx_id = get_request_param('tx_id')
+
+    tx = {'cid': 0, 'txid': tx_id}
+
+    store_result = bigchain_client.sign_transaction(public_key, private_key, send_to_key, tx)
+
+    return json.dumps({'result': store_result})
+
+
+@route('/app/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='/vagrant/bb/app')
 
 
 def get_asset_payload(asset):
--- bigchain_client.py
+++ bigchain_client.py
@@ -11,8 +11,22 @@ def store_asset(public_key, digital_asset_payload):
     tx = b.create_transaction(b.me, public_key, None, 'CREATE', payload=digital_asset_payload)
 
     tx_signed = b.sign_transaction(tx, b.me_private)
+    b.write_transaction(tx_signed)
 
-    return b.write_transaction(tx_signed)
+    return tx_signed
+
+
+def sign_transaction(public_key, private_key, send_to_key, tx_id):
+    # Create a transfer transaction
+    tx_transfer = b.create_transaction(public_key, send_to_key, tx_id, 'TRANSFER')
+
+    # Sign the transaction
+    tx_transfer_signed = b.sign_transaction(tx_transfer, private_key)
+
+    # Write the transaction
+    b.write_transaction(tx_transfer_signed)
+
+    return tx_transfer_signed
 
 
 def exists(digital_asset_payload):
@@ -21,5 +35,20 @@ def exists(digital_asset_payload):
     transactions = b.get_tx_by_payload_hash(payload_hash)
     result = transactions is not None and len(transactions) > 0
 
-    return result
+    return result, transactions
+
+
+def get_owned_ids(public_key):
+# transactions = b.get_owned_ids(public_key)
+#
+# return transactions
+    transactions_ids = b.get_owned_ids(public_key)
+    transactions = []
+
+    for tx in transactions_ids:
+        full_transaction = b.get_transaction(tx['txid'])
+        transactions.append(full_transaction)
+
+    return transactions
+
 
